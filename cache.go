d6b28e2575c74372d0d22c3f581871f1edace4ae
Andrew Mysliuk <43147947+AndrewMislyuk@users.noreply.github.com>
2022-03-07 00:31:32 +0200

    Update cache.go

--- cache.go
+++ cache.go
@@ -2,17 +2,17 @@ package cache
 
 import "fmt"
 
-type Cache map[string]interface{}
+type cache map[string]interface{}
 
-func New() Cache {
-	return Cache{}
+func New() cache {
+	return cache{}
 }
 
-func (m Cache) Set(key string, value interface{}) {
+func (m cache) Set(key string, value interface{}) {
 	m[key] = value
 }
 
-func (m Cache) Get(key string) (value interface{}) {
+func (m cache) Get(key string) (value interface{}) {
 	value, ok := m[key]
 	if !ok {
 		return fmt.Sprintln("Key doesn't exist")
@@ -21,7 +21,7 @@ func (m Cache) Get(key string) (value interface{}) {
 	return value
 }
 
-func (m Cache) Delete(key string) {
+func (m cache) Delete(key string) {
 	_, ok := m[key]
 	if ok {
 		delete(m, key)
